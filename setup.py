172bc9e3de7c9d4a12cba3a655e4d8dda9ca72fa
MkLHX <mickael.lehoux@gmail.com>
2020-07-23 16:23:57 +0200

    add version file and changes on setup file

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 from setuptools import setup, find_packages
 import pathlib
+import os
 
 here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
+
+def load_version():
+    version_file = os.path.join(os.path.dirname(
+        __file__), "src/GreenPonik_OneWire_DS18B20", "version.py")
+    version = {}
+    with open(version_file) as fd:
+        exec(fd.read(), version)
+    return version["__version__"]
+
+
 setup(
     name="greenponik-tsl2561",
-    version="0.0.1",
+    version=load_version(),
     author="GreenPonik SAS",
-    author_email="mickael.lehoux@greenponik.com",
+    author_email="contact@greenponik.com",
     description="Read TSL2561 through Python3 on raspberry pi",
     long_description=long_description,
     long_description_content_type="text/markdown",
