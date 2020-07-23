97bcd151d179ef1f706c8657d7857eb008e8e7ea
MkLHX <mickael.lehoux@gmail.com>
2020-07-23 14:46:45 +0200

    manage setup content

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="GreenPonik_TSL2561",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="GreenPonik SAS",
+    author_email="mickael.lehoux@greenponik.com",
+    description="Read TSL2561 through Python3 on raspberry pi",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/GreenPonik/GreenPonik_TSL2561",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
