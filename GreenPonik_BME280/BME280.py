58e0a0144e6c76ce045aa48f40207ba5f57bd97d
Mickael <mickael.lehoux@greenponik.com>
2020-09-14 18:42:49 +0200

    provide default value for debug

--- GreenPonik_BME280/BME280.py
+++ GreenPonik_BME280/BME280.py
@@ -23,7 +23,7 @@ class BME280:
     def __init__(self, bus=DEFAULT_BUS, addr=DEFAULT_ADDR):
         self._bus = bus
         self._addr = addr
-        self._debug
+        self._debug = False
 
     @property
     def bus(self):
